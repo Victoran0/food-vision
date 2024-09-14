04027a3aa9ed3ccf0e4f65c8a30066126626dd40
Victoran0 <victoranosunkvic@gmail.com>
2024-09-14 04:27:44 +0100

    Loaded tensorboard and saved best model

--- predict.py
+++ predict.py
@@ -1,57 +1,85 @@
-from typing import List, Tuple
-import matplotlib.pyplot as plt
-from PIL import Image
+"""
+Utility functions to make predictions.
+
+Main reference for code creation: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set 
+"""
 import torch
 import torchvision
+from torchvision import transforms
+import matplotlib.pyplot as plt
 
-# 1. Take in a trained model, class names, image path, image size, a transform and target device
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+from typing import List, Tuple
+
+from PIL import Image
+
+# Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Predict on a target image with a target model
+# Function created in: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set
 
 
-def pred_and_plot_image(model: torch.nn.Module,
-                        image_path: str,
-                        class_names: List[str],
-                        image_size: Tuple[int, int] = (224, 224),
-                        transform: torchvision.transforms = None,
-                        device: torch.device = device):
+def pred_and_plot_image(
+    model: torch.nn.Module,
+    class_names: List[str],
+    image_path: str,
+    image_size: Tuple[int, int] = (224, 224),
+    transform: torchvision.transforms = None,
+    device: torch.device = device,
+):
+    """Predicts on a target image with a target model.
 
-    # 2. Open image
+    Args:
+        model (torch.nn.Module): A trained (or untrained) PyTorch model to predict on an image.
+        class_names (List[str]): A list of target classes to map predictions to.
+        image_path (str): Filepath to target image to predict on.
+        image_size (Tuple[int, int], optional): Size to transform target image to. Defaults to (224, 224).
+        transform (torchvision.transforms, optional): Transform to perform on image. Defaults to None which uses ImageNet normalization.
+        device (torch.device, optional): Target device to perform prediction on. Defaults to device.
+    """
+
+    # Open image
     img = Image.open(image_path)
 
-    # 3. Create transformation for image (if one doesn't exist)
+    # Create transformation for image (if one doesn't exist)
     if transform is not None:
         image_transform = transform
     else:
-        image_transform = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225]),
-        ])
+        image_transform = transforms.Compose(
+            [
+                transforms.Resize(image_size),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
 
     ### Predict on image ###
 
-    # 4. Make sure the model is on the target device
+    # Make sure the model is on the target device
     model.to(device)
 
-    # 5. Turn on model evaluation mode and inference mode
+    # Turn on model evaluation mode and inference mode
     model.eval()
     with torch.inference_mode():
-        # 6. Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
+        # Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
         transformed_image = image_transform(img).unsqueeze(dim=0)
 
-        # 7. Make a prediction on image with an extra dimension and send it to the target device
+        # Make a prediction on image with an extra dimension and send it to the target device
         target_image_pred = model(transformed_image.to(device))
 
-    # 8. Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
+    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
-    # 9. Convert prediction probabilities -> prediction labels
+    # Convert prediction probabilities -> prediction labels
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+    print('Label: ', target_image_pred_label)
 
-    # 10. Plot image with predicted label and probability
+    # Plot image with predicted label and probability
     plt.figure()
     plt.imshow(img)
-    plt.title(
-        f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}")
+    # plt.title(
+    #     f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}"
+    # )
     plt.axis(False)
---
+++ set_seeds.py
@@ -0,0 +1,15 @@
+import torch
+
+# Set seeds
+
+
+def set_seeds(seed: int = 42):
+    """Sets random sets for torch operations.
+
+    Args:
+        seed (int, optional): Random seed to set. Defaults to 42.
+    """
+    # Set the seed for general torch operations
+    torch.manual_seed(seed)
+    # Set the seed for CUDA torch operations (ones that happen on the GPU)
+    torch.cuda.manual_seed(seed)
